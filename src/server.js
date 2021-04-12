12707be66e74e82879bfebe7efea56064039ab75
Triyono <yono.tri@gmail.com>
2021-04-12 11:34:45 +0700

    add hapi

--- src/server.js
+++ src/server.js
@@ -1,1 +1,12 @@
-console.log('hallo');
+const Hapi = require('@hapi/hapi');
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 5000,
+    host: 'localhost',
+  });
+  await server.start();
+  console.log(`Server berjalanan pada ${server.info.uri}`);
+};
+
+init();

81ada995433159cd61b17382f293f2309e7d81d2
Triyono <yono.tri@gmail.com>
2021-04-12 12:43:33 +0700

    implement to server.js

--- src/handler.js
+++ src/handler.js
@@ -1,7 +1,7 @@
 const {nanoid} = require('nanoid');
 const notes = require('./notes');
 
-const addNoteHandler = (request, h)=>{
+const addNoteHandler = (request, h) => {
   const {title = 'untitled', tags, body} = request.payload;
   const id = nanoid(16);
   const createdAt = new Date().toISOString();
@@ -10,6 +10,27 @@ const addNoteHandler = (request, h)=>{
     title, tags, body, id, createdAt, updatedAt,
   };
   notes.push(newNote);
+
+  const isSuccess = notes.filter((note) => note.id === id).length > 0;
+  if (isSuccess) {
+    const response = h.response({
+      status: 'success',
+      message: 'Catatan berhasil ditambahkan',
+      data: {
+        noteId: id,
+      },
+
+    });
+    response.code(201);
+    return response;
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Catatan gagal ditambahkan',
+  });
+  response.code(500);
+  return response;
 };
 
 module.exports = {addNoteHandler};

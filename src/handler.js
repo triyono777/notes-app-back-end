aba73632fa33fa8fd8043080086e0e47b5fe12eb
Triyono <yono.tri@gmail.com>
2021-04-12 11:45:01 +0700

    add newNote

--- src/handler.js
+++ src/handler.js
@@ -1,5 +1,15 @@
+const {nanoid} = require('nanoid');
+const notes = require('./notes');
+
 const addNoteHandler = (request, h)=>{
   const {title = 'untitled', tags, body} = request.payload;
+  const id = nanoid(16);
+  const createdAt = new Date().toISOString();
+  const updatedAt = createdAt;
+  const newNote = {
+    title, tags, body, id, createdAt, updatedAt,
+  };
+  notes.push(newNote);
 };
 
 module.exports = {addNoteHandler};
